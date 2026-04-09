c0b1ef7a14feb0b18d147c8e092882d0782739cb
chris <chris@chriss-MacBook-Air.local>
2026-04-09 11:35:17 -0700

    treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,11 +1,16 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	opts = {
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-	},
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  lazy = false,
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter').install { 'astro', 'css', 'glsl', 'go', 'html', 'javascript', 'json', 'jsx', 'lua', 'markdown', 'odin', 'tmux', 'tsx', 'typescript' }
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'astro', 'css', 'glsl', 'go', 'html', 'javascript', 'json', 'jsx', 'lua', 'markdown', 'odin', 'tmux', 'tsx', 'typescript' },
+      callback = function()
+        vim.treesitter.start()
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
