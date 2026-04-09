0897a699d4a9c290a6e5d39962045f329bf620dd
chris <chris.schoony@gmail.com>
2026-04-09 12:42:39 -0700

    update treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,46 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  lazy = false,
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter').install { 'astro', 'css', 'glsl', 'go', 'html', 'javascript', 'json', 'jsx', 'lua', 'markdown', 'odin', 'tmux', 'tsx', 'typescript' }
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'astro', 'css', 'glsl', 'go', 'html', 'javascript', 'json', 'jsx', 'lua', 'markdown', 'odin', 'tmux', 'tsx', 'typescript' },
-      callback = function()
-        vim.treesitter.start()
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      end,
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").install({
+			"astro",
+			"css",
+			"glsl",
+			"go",
+			"html",
+			"javascript",
+			"json",
+			"jsx",
+			"lua",
+			"markdown",
+			"odin",
+			"tmux",
+			"tsx",
+			"typescript",
+		})
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = {
+				"astro",
+				"css",
+				"glsl",
+				"go",
+				"html",
+				"javascript",
+				"json",
+				"jsx",
+				"lua",
+				"markdown",
+				"odin",
+				"tmux",
+				"tsx",
+				"typescript",
+			},
+			callback = function()
+				vim.treesitter.start()
+				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+			end,
+		})
+	end,
 }
