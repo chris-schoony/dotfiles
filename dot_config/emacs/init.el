75581f91c5676576ac4f006aa742d941d72e698c
chris <chris.schoony@gmail.com>
2026-09-12 21:54:31 -0700

    Add git-gutter-fringe

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -75,6 +75,17 @@
 (use-package magit
   :ensure t)
 
+(use-package git-gutter-fringe
+  :ensure t
+  :config
+  (setq-default fringes-outside-margins t)
+  (define-fringe-bitmap 'git-gutter-fr:added [224]
+    nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224]
+    nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
+    nil nil 'bottom))
+
 ;; BUILT IN PACKAGES CONFIG
 (use-package savehist
   :ensure nil
