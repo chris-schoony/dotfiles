fb91c5c18f6b51f2f8994ce289628130ea6f06f2
chris <chris.schoony@gmail.com>
2026-09-12 23:12:18 -0700

    add magit hook to git-gutter-fringe

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -84,7 +84,9 @@
   (define-fringe-bitmap 'git-gutter-fr:modified [224]
     nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240]
-    nil nil 'bottom))
+    nil nil 'bottom)
+  (with-eval-after-load 'magit
+    (add-hook 'magit-post-refresh-hook #'git-gutter:update-all-windows)))
 
 (use-package vterm
   :ensure t
